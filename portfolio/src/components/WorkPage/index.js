d3e4de8d5534e6fbe4044716a9e7a85444df488c
shark27 <44381346+jslzr27@users.noreply.github.com>
2019-06-26 16:05:24 -0400

    changes made

--- portfolio/src/components/WorkPage/index.js
+++ portfolio/src/components/WorkPage/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import "./style.css";
+
+import timi from "../../portfolio things/time 2.png";
+import fott from "../../portfolio things/clickygame 54.png";
+import pets from "../../portfolio things/pets.png";
+import burg from "../../portfolio things/burger 4.png";
+import bama from "../../portfolio things/tablebamazon.png";
+import liri from "../../portfolio things/liri1 313.png";
+import mme from "../../portfolio things/movie 118.png";
+import trai from "../../portfolio things/trainTime 0.png";
+import crys from "../../portfolio things/crystalCollect 0.png";
 
 class WorkPage extends Component {
     render() {
         return(
             <div className="section" id="work">
-                <h1>WorkPage</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida consequat sem, vitae consectetur sapien imperdiet a. Sed tristique lacinia arcu, non fringilla mi dapibus at. Cras vel arcu sit amet ex maximus consectetur vitae quis eros. In commodo elit ac nulla maximus pretium. Vivamus nec rhoncus arcu. Duis ultricies mattis enim, in sodales dolor dignissim vitae. Vivamus ante nisl, sollicitudin ac sem in, dignissim laoreet ex. Etiam eget leo vel metus ultricies malesuada. Suspendisse sagittis fringilla purus, nec tristique est eleifend vitae.
-
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras in ligula pellentesque, cursus felis at, congue ante. Mauris bibendum ipsum lectus, eget aliquet diam laoreet et. Phasellus bibendum facilisis consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec euismod in sem in semper. Proin feugiat tempor risus, sed lobortis nibh blandit nec. Morbi mollis, mi ac consequat malesuada, felis ligula scelerisque ex, vel maximus justo lectus at risus. Morbi vel feugiat dolor, nec pretium metus. Cras purus purus, euismod nec faucibus et, porttitor at nisl. Integer dolor quam, molestie id ex et, posuere malesuada nisl. Quisque suscipit lorem at maximus laoreet. Proin pulvinar a nisl non vehicula. Aenean sit amet risus ac arcu gravida vulputate feugiat vel arcu. Cras sed pulvinar magna, non congue elit.
-
-Nullam faucibus cursus mollis. Ut vitae rutrum metus, non imperdiet nisi. Nam nisl ipsum, suscipit et nunc hendrerit, convallis volutpat turpis. Suspendisse commodo vulputate lacus, vitae tristique mauris lacinia at. Cras metus dolor, ultrices vitae elementum nec, congue sit amet justo. Nunc quis convallis nunc. Ut sed velit in turpis hendrerit viverra id ac erat. Curabitur vulputate placerat nisi vel congue. Nulla facilisi. Curabitur eget blandit urna. Pellentesque nulla ligula, euismod lobortis laoreet a, feugiat sed nibh. Nunc finibus eu nisl quis suscipit.
-
-Donec faucibus nisi id eleifend eleifend. Curabitur non auctor dui. Etiam egestas enim enim, sit amet efficitur nulla dapibus eget. Sed scelerisque, nunc et vulputate gravida, mi est auctor tortor, vitae commodo lacus sem et quam. Suspendisse at tellus accumsan, imperdiet justo ut, ultrices arcu. Duis ultricies justo in risus rhoncus, id finibus purus semper. Quisque vel purus sit amet nunc scelerisque posuere.
-
-Suspendisse finibus justo arcu. Sed enim sem, tincidunt vel finibus eu, rhoncus nec tellus. Aenean molestie pretium mauris, vel posuere erat rutrum nec. Nulla efficitur accumsan sem, mollis iaculis orci molestie eget. Nulla cursus, justo sit amet condimentum vestibulum, sapien leo dignissim dolor, ac lacinia tellus quam ut libero. Quisque posuere mi sed pulvinar sagittis. Nulla mollis ante sit amet leo pellentesque, placerat lobortis elit tristique. Sed vitae auctor ante. Integer a imperdiet enim, id convallis nulla. Pellentesque risus leo, pellentesque quis scelerisque sed, aliquam nec nisl.
-</p>
+                <h1>My Projects</h1>
+                
+                <div className="workList">
+                    <li><img src={timi} className="workImg" alt="time management"></img>
+                    <div className="overlay">
+                        <div className="text">Hello World</div></div>Time Management App</li>
+                    <li><img src={fott} className="workImg" alt="Football clicky game"></img>Football Clicky Game</li>
+                    <li><img src={pets} className="workImg" alt="Petspurration"></img>Petspurration</li>
+                    <li><img src={burg} className="workImg" alt="Burger App"></img>Burger App</li>
+                    <li><img src={bama} className="workImg" alt="Node inventory management"></img>Node + MySQL Inventory Management</li>
+                    <li><img src={liri} className="workImg" alt="Liri Bot"></img>Liri Bot</li>
+                    <li><img src={mme}  className="workImg" alt="Movie Music Enthusiast" data-text="mme2"></img>Movie Music Enthusiast</li>
+                    <li><img src={trai} className="workImg" alt="Train Time"></img>Train Time</li>
+                    <li><img src={crys} className="workImg" alt="Crystals Game"></img>Crystals Game</li>
+                </div>
             </div>
         )
     }
